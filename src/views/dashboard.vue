df8e7721f386d3e69582b00d93d9fe8e18e058cb
nokranger <anusorn.thavornpoorn@gmail.com>
2023-12-21 14:44:14 +0700

    tnos all data post

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -15,10 +15,10 @@
       <div>
         Instructor Data
       </div>
-      <input type="file" ref="fileInput" @change="handleFileChange" />
-      <button @click="exportToExcel">Export to Excel</button>
+      <input type="file" ref="fileInput" @change="handleFileChangeInstructor" />
+      <!-- <button @click="exportToExcel">Export to Excel</button>
       <br><br>
-      <button @click="PersonalSendData">Instructor Data</button>
+      <button @click="PersonalSendData">Instructor Data</button> -->
     </div>
     <br>
     <br>
@@ -34,6 +34,8 @@
     <br>
     <br>
     <div>
+      <br>
+      <br>
       <div>
         Welfare Data
       </div>
@@ -41,6 +43,15 @@
       <button @click="exportToExcelWelfare">Export to Excel</button>
       <br><br>
       <button @click="WelfareSendData">Welfare Data</button>
+    </div>
+    <div>
+      <div>
+        Master Data
+      </div>
+      <!-- <input type="file" ref="fileInput" @change="handleFileChangeWelfare" />
+      <button @click="exportToExcelWelfare">Export to Excel</button> -->
+      <br><br>
+      <button @click="MasterSendData">MasterSendData</button>
     </div>
     <!-- <pre v-if="jsonData">{{ jsonData }}</pre> -->
   </div>
@@ -66,8 +77,11 @@ export default {
       jsondataTnos2: null,
       jsondataTnos3: null,
       jsondataTnos4: null,
+      jsondataTnos5: null,
       jsonArrayTnos: [],
       excelarrayTnos: [],
+      jsonDataInstructor: null,
+      jsonDataInstructor2: null,
       testdata: ''
     };
   },
@@ -91,6 +105,51 @@ export default {
             })
             console.log('resdataExcel', jsonMaps);
             this.excelarray = Object.values(jsonMaps);
+            console.log('JSONTYPEOF2Aftermap23', this.excelarray)
+
+            //export to excell
+            const workbook = XLSX.utils.book_new();
+            
+            // Convert the JSON data to a worksheet
+            const worksheet = XLSX.utils.json_to_sheet(this.excelarray);
+
+            // Add the worksheet to the workbook
+            XLSX.utils.book_append_sheet(workbook, worksheet, 'MasterData');
+
+            // Save the workbook to a file
+            XLSX.writeFile(workbook, 'exported_data.xlsx');
+            })
+      .catch(error => {
+        console.error('Error fetching data:', error.message);
+      });
+      // console.log(this.testdata)
+      // axios.post('http://localhost:4000/personal', this.testdata)
+      // .then(response => {
+      //   console.log(response.data);
+      // })
+      // .catch(error => {
+      //   console.error('Error fetching data:', error.message);
+      // });
+    },
+    MasterSendData () {
+      this.testdata = {
+        emp_code: '12345',
+        name: 'NAMAMAMAM',
+        bank_account_number: '12344566'
+      }
+      axios.get('http://localhost:4000/masterdata')
+      .then(response => {
+        console.log('resdata', response.data.result);
+        let dataexcel = response.data.result
+        // let jsonMaps = dataexcel.map((data, i) => {
+        //       return {
+        //         "Emp. Code": data.emp_code,
+        //         "Name - Surname": data.name,
+        //         "Bank account number": data.bank_account_number
+        //       }
+        //     })
+            // console.log('resdataExcel', jsonMaps);
+            this.excelarray = Object.values(dataexcel);
             console.log('JSONTYPEOF2Aftermap23', this.excelarray)
 
             //export to excell
@@ -455,28 +514,189 @@ export default {
                 allowance_payment_date: data.item44,
               }
             })
+            let jsonMapTnos5 = jsonobjectTnos.map((data, i) => {
+              return {
+                Working_date: data.item1,
+                job_code: data.item2,
+                shift: data.item3,
+                trip_no: data.item4,
+                ttt_employee_code: data.item5,
+                tlep_driver_code: data.item6,
+                tlep_driver_name: data.item7,
+                company_code: data.item8,
+                company_name: data.item9,
+                trailer_code: data.item10,
+                trailer_type_code: data.item11,
+                trailer_type: data.item12,
+                ttt_payment_status: data.item13,
+                calling_sheet_no: data.item14,
+                trip_type: data.item15,
+                recieve_job_dateandtime: data.item16,
+                from_code: data.item17,
+                from_name: data.item18,
+                yard_out_dateandtime: data.item19,
+                to_code: data.item20,
+                to_name: data.item21,
+                to_in_dateandtime: data.item22,
+                reture_code: data.item23,
+                return_name: data.item24,
+                return_in_dateandtime: data.item25,
+                loading_units: data.item26,
+                loading_count: data.item27,
+                unloading_count: data.item28,
+                number_of_driver: data.item29,
+                nd2_employee_code: data.item30,
+                nd2_tlep_driver_code: data.item31,
+                nd2_tlep_driver_name: data.item32,
+                mileage: data.item33,
+                allowance: data.item34,
+                allowance2: data.item35,
+                allowance3: data.item36,
+                allowance4: data.item37,
+                total_allowance: data.item38,
+                standard_ot: data.item39,
+                over_ot: data.item40,
+                total_ot: data.item41,
+                payment_status: data.item42,
+                ot_payment_date: data.item43,
+                allowance_payment_date: data.item44,
+              }
+            })
         console.log('Aftermap', jsonobjectTnos)
         console.log('Aftermap2', jsonMapTnos)
         console.log('Aftermap22', jsonMapTnos2)
         console.log('Aftermap23', jsonMapTnos3)
         console.log('Aftermap24', jsonMapTnos4)
-        // console.log('JSONTYPEOF2Aftermap: ',  typeof(jsonobjectTnos))
-        // this.jsondata2Tnos = jsonMapTnos
-        // this.jsondata2Tnos2 = jsonMapTnos2
-        // axios.post('http://localhost:4000/Tnos', this.jsondata2Tnos)
+        console.log('JSONTYPEOF2Aftermap: ',  typeof(jsonobjectTnos))
+        this.jsondata2Tnos = jsonMapTnos
+        this.jsondata2Tnos2 = jsonMapTnos2
+        this.jsondata2Tnos3 = jsonMapTnos3
+        this.jsondata2Tnos4 = jsonMapTnos4
+        this.jsondata2Tnos5 = jsonMapTnos5
+        axios.post('http://localhost:4000/tnos5', this.jsondata2Tnos5)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error.message);
+        });
+        // axios.post('http://localhost:4000/tnos2', this.jsondata2Tnos2)
         // .then(response => {
         //   console.log(response.data);
         // })
         // .catch(error => {
         //   console.error('Error fetching data:', error.message);
         // });
-        // axios.post('http://localhost:4000/Tnos2', this.jsondata2Tnos2)
+        // axios.post('http://localhost:4000/tnos3', this.jsondata2Tnos3)
         // .then(response => {
         //   console.log(response.data);
         // })
         // .catch(error => {
         //   console.error('Error fetching data:', error.message);
         // });
+        // axios.post('http://localhost:4000/tnos4', this.jsondata2Tnos4)
+        // .then(response => {
+        //   console.log(response.data);
+        // })
+        // .catch(error => {
+        //   console.error('Error fetching data:', error.message);
+        // });
+      };
+
+      reader.readAsBinaryString(file);
+    },
+    handleFileChangeInstructor(event) {
+      console.log('Instructor')
+      const file = event.target.files[0];
+
+      if (file) {
+        this.readExcelInstructor(file);
+      }
+    },
+    readExcelInstructor(file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: 'binary' });
+
+        // Assume the first sheet is the one you want to read
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+
+        // Convert the sheet data to JSON
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        // Set the JSON data to be displayed in the component
+        this.jsonDataInstructor = JSON.stringify(jsonData, null, 2);
+        this.jsonDataInstructor = JSON.parse(this.jsonDataInstructor)
+        this.jsonDataInstructor.shift()
+        // console.log('mapdata1: ', this.jsonData)
+        console.log('JSONLenght: ',  this.jsonDataInstructor)
+        // console.log('JSONLenghtSHIFT: ',  JSON.parse(this.jsonData).shift())
+        console.log('JSONTYPEOF: ',  typeof(this.jsonDataInstructor))
+        // console.log('JSONTYPEOF2: ',  typeof(JSON.parse(this.jsonData)))
+        let jsonobjectInstructor = {}
+        let jsonobject2Instructor = this.jsonDataInstructor
+        jsonobjectInstructor = jsonobject2Instructor.map(innerarray => {
+          return innerarray.reduce((acc, item, index) => {
+            acc[`item${index + 1}`] = item;
+            return acc
+          }, {})
+        })
+        let jsonMapInstructor = jsonobjectInstructor.map((data, i) => {
+              return {
+                number: data.item1,
+                TRIP_NO: data.item2,
+                TRIP_ALLOWANCE: data.item3,
+                TOTAL_ALLOWANCE: data.item4,
+                NULL1: data.item5,
+                NULL2: data.item6,
+                DEPARTURE_DATETIME: data.item7,
+                DEPARTURE_DATETIME2: data.item8,
+                DRIVER1: data.item9,
+                NAME: data.item10,
+                NULL3: data.item11,
+                NULL4: data.item12,
+
+              }
+            })
+            let jsonMapInstructor2 = jsonobjectInstructor.map((data, i) => {
+              return {
+                TRIP_NO: data.item2,
+                DEALER1: data.item13,
+                DEALER2: data.item14,
+                DEALER3: data.item15,
+                DEALER4: data.item16,
+                DEALER5: data.item17,
+                UNITS1: data.item18,
+                UNITS2: data.item19,
+                UNITS3: data.item20,
+                UNITS4: data.item21,
+                UNITS5: data.item22,
+                TAX_FLAG: data.item23,
+              }
+            })
+        console.log('Aftermap', jsonobjectInstructor)
+        console.log('Aftermap2', jsonMapInstructor)
+        console.log('Aftermap22', jsonMapInstructor2)
+        console.log('JSONTYPEOF2Aftermap: ',  typeof(jsonobjectInstructor))
+        this.jsondata2Instructor = jsonMapInstructor
+        this.jsondata2Instructor2 = jsonMapInstructor2
+        axios.post('http://localhost:4000/instructor', this.jsondata2Instructor)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error.message);
+        });
+        axios.post('http://localhost:4000/instructor2', this.jsondata2Instructor2)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error.message);
+        });
       };
 
       reader.readAsBinaryString(file);
