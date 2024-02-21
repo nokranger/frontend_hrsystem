9c19913d226ddd88affb05c9adf67f88071074ec
nokranger <anusorn.thavornporn@gmail.com>
2024-02-21 18:18:50 +0700

    ref

--- src/views/pdf.vue
+++ src/views/pdf.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
     <b-container>
-      <div style="border: 2px solid gray;border-radius: 10px;height: 29.7cm;width: 21cm;box-shadow: 5px 5px 5px #888888;">
+      <div id="content" style="border: 2px solid gray;border-radius: 10px;height: 29.7cm;width: 21cm;box-shadow: 5px 5px 5px #888888;">
         <div style="margin-top: 10px;">TESTPDF</div>
       </div>
     </b-container>
+    <button @click="testpdf()">Test</button>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    testpdf () {
+      console.log("testpdf")
+    }
+  },
+}
+</script>
